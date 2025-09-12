32ae477a074ee2e55b6af307a49e54cde295aa74
heidi3115 <heidi3115@gmail.com>
2025-09-13 01:59:58 +0900

    prettier, eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module"
     },
-  },
-])
+    plugins: {
+      "@typescript-eslint": ts,
+      react,
+      "react-hooks": reactHooks,
+      prettier
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "error",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+];
