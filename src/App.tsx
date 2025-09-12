f906ea8293a2bf5aa9a57990455e7838bd7d52cb
mskim94 <mskim@jasonsystem.co.kr>
2025-09-12 16:19:19 +0900

    Initialize project with Vite, React, TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import styled from "@emotion/styled";
+
+const TestCss = styled.div`
+    border: 1px solid red;
+    background: gray;
+    padding: 10px;
+    color: #fff;
+`
+
+
+function App() {
+  return (
+    <>
+      <TestCss>
+       first commit
+      </TestCss>
+    </>
+  )
+}
+
+export default App
